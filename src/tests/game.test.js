e692344bb5043fbc179959b1783882660e077cf1
Nick <nicktelindert@protonmail.com>
2022-08-18 12:42:07 +0200

    Improve testing by using spies

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -1,7 +1,8 @@
 import game from '../composables/models/game.js';
+import product from '../composables/models/product.js';
 import ship from '../composables/models/ship.js';
 import player from '../composables/models/player.js';
-const productName = 'Purple Bananas';
+
 import {jest} from '@jest/globals';
 import planet from '../composables/models/planet';
 
@@ -12,35 +13,59 @@ const shipModel = {
   'capacityUsed': 0,
   'capacityFull': 100,
 };
-
-planet.getCurrentPlanet = (() => {
-  return {
-    'name': 'Xena',
-    'market': [],
-  };
-});
-
-planet.getMarketProductByName = jest.fn().mockReturnValue({
-  name: productName,
-  price: 123,
-});
-
-ship.purchaseShip = jest.fn().mockReturnValue(shipModel);
+const productName = 'Purple Bananas';
 
 
-player.getHumanPlayer = jest.fn().mockReturnValue({
-  'name': 'Player',
-  'ai': false,
-  'balance': 20000,
-  'ship': shipModel,
-});
+beforeAll (() => {
+  jest.spyOn(localStorage, 'setItem').mockReturnValue(true)
+  jest.spyOn(localStorage, 'getItem').mockReturnValue()
+  jest.spyOn(planet, 'getMarketProductByName').mockReturnValue({
+    name: productName,
+    price: 12.50
+  })
 
-player.getPlayerList = jest.fn().mockReturnValue([{
-  'name': 'Computer',
-  'ai': true,
-}]);
+  jest.spyOn(planet, 'getCurrentPlanet').mockReturnValue({ name: 'Xena', market: []})
+  jest.spyOn(planet, 'getMarketProductByName').mockReturnValue({'name': productName, 'quantity': 15, 'price': 10 })
+  jest.spyOn(planet, 'getPlanetList').mockReturnValue([{'name': 'Xena', 'market': [{'name': productName, 'quantity': 15, 'price': 10 }]}])
+  jest.spyOn(planet, 'setCurrentPlanet').mockReturnThis()
 
-game.createNewGame('Player One', ship.purchaseShip('Ship'));
+  jest.spyOn(product, 'updateQuantity').mockReturnThis()
+ 
+
+  jest.spyOn(player, 'updateBalance').mockReturnThis()
+  jest.spyOn(player, 'checkForWinners').mockReturnValue(true)
+  jest.spyOn(player, 'getNonHumanPlayers').mockReturnValue([{'name': 'computer', 'ai': true, 'ship': shipModel}])
+  jest.spyOn(player, 'isThereAHumanPlayer').mockReturnValue(true)
+
+  jest.spyOn(player, 'updatePlayer').mockReturnThis()
+  jest.spyOn(player,'getPlayerList').mockReturnValue([{
+    'name': 'Computer',
+    'ai': true,
+  }]);
+
+  jest.spyOn(ship, 'addProductToCargo').mockReturnThis()
+  jest.spyOn(ship, 'getProductFromCargo').mockReturnValue({'name': productName, 'quantity': 15 })
+  jest.spyOn(ship, 'updateProductQuantityInCargo').mockReturnThis()
+  jest.spyOn(ship, 'removeProductFromCargo').mockReturnThis()
+  jest.spyOn(ship, 'purchaseShip').mockReturnValue(shipModel)
+
+  jest.spyOn(game, 'sellProduct')
+
+
+  game.createNewGame('Player One', ship.purchaseShip('Ship'));
+  
+})
+
+beforeEach(() => {
+  jest.spyOn(player, 'getHumanPlayer').mockReturnValue({ 
+    'name': 'Player',
+    'ai': false,
+    'balance': 20000,
+    'ship': shipModel
+  })
+})
+
+
 test('Game round should be equal to 0', () => {
   game.startGame(true);
   expect(game.round).toEqual(0);
@@ -57,6 +82,16 @@ test('Buy a product should now return false', () => {
   expect(game.buyProduct(productName, 10000000, player.getHumanPlayer())).toBeFalsy();
 });
 
+test ('Buy a product should return false becuase of insufficient balance', () => {
+  jest.spyOn(player, 'getHumanPlayer').mockReturnValue({ 
+    'name': 'Player',
+    'ai': false,
+    'balance': 0,
+    'ship': shipModel
+  })
+  expect(game.buyProduct(productName, 13, player.getHumanPlayer())).toBeFalsy();
+
+})
 test('Sell product should return false', () => {
   game.startRound('Xena');
   expect( game.sellProduct(productName, 19, player.getHumanPlayer())).toBeFalsy();
@@ -72,6 +107,13 @@ test('Sell product should return true', () => {
   expect( game.sellProduct(productName, 12, player.getHumanPlayer())).toBeTruthy();
 });
 
+test('Sell product should call ship.removeProductFromCargo', () => {
+  game.startRound('Xena');
+  game.sellProduct(productName, 15, player.getHumanPlayer())
+  expect(ship.removeProductFromCargo).toBeCalled();
+});
+
+
 test('endRound should return false', () => {
   for (let t=0; t<11; t++) {
     game.startRound('Xena');
@@ -80,6 +122,36 @@ test('endRound should return false', () => {
   expect(game.endRound()).toEqual(false);
 });
 
+test ('player.checkForWinnners should be called', () => {
+  jest.spyOn(localStorage, 'getItem').mockReturnValue(12);
+  game.endRound();
+  expect(player.checkForWinners).toBeCalled();
+})
+
+
+test ('endRound should return false because there is no humanplayer left', () => {
+  jest.spyOn(localStorage, 'getItem').mockReturnValue(12);
+  jest.spyOn(player,'isThereAHumanPlayer').mockReturnValue(false);
+  game.endRound();
+  expect(game.endRound()).toBeFalsy();
+})
+
+test ('endRound should return false because there is no winner', () => {
+  jest.spyOn(localStorage, 'getItem').mockReturnValue(12);
+  jest.spyOn(player,'checkForWinners').mockReturnValue(false);
+  game.endRound();
+  expect(game.endRound()).toBeFalsy();
+})
+
+
 test('letAiPlay should return true', () => {
   expect(game.letAiPlay()).toBeTruthy();
+});
+
+
+test('letAiPlay should call sellProduct', () => {
+  shipModel.cargo = [{name: productName, price: 12.50, quantity: 12}]
+  jest.spyOn(player, 'getNonHumanPlayers').mockReturnValue([{'name': 'computer', 'ai': true, 'ship': shipModel}])
+  game.letAiPlay()
+  expect(game.sellProduct).toBeCalled();
 });
